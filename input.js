563c64b65a16482216fe9b56529feada6453f379
Iain Mcilveen <45949570+IainMcilveen@users.noreply.github.com>
2019-06-22 16:59:49 -0300

    Initial commit

---
+++ input.js
@@ -0,0 +1,10 @@
+function name(){
+    var name;
+    name = document.getElementById("name").value;
+    console.log("name: "+name);
+}
+function password(){
+    var password;
+    password = document.getElementsById("password").value;
+    console.log("password: "+password);
+}
